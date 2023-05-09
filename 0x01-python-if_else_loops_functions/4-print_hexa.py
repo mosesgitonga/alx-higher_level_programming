fd067a028bce81c62581746870992a598dde0a9c
mosesgitonga <infosec947@gmail.com>
2023-05-09 03:11:41 -0700

    removed unnecessary printf

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(0, 99):
-	if i < 10:
-		print("{} = 0x0{:x}".format(i, i))
-	elif i >= 10 and i <= 15:
-		print("{} = 0x{:x}".format(i, i))
-	else:
-		print("{} = 0x{:x}".format(i, i))
+	print("{} = 0x{:x}".format(i, i))
