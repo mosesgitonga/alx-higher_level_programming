8f93a843bc52984be367e73a467b43c8f7c99412
mosesgitonga <infosec947@gmail.com>
2023-05-09 03:08:35 -0700

    fixing requirement

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,8 +2,8 @@
 
 for i in range(0, 99):
 	if i < 10:
-		print(f"{i} = 0x0{i:x}")
+		print("{} = 0x0{:x}".format(i, i))
 	elif i >= 10 and i <= 15:
-		print(f"{i} = 0x{i:x}")
+		print("{} = 0x{:x}".format(i, i))
 	else:
-		print(f"{i} = 0x{i:x}")
+		print("{} = 0x{:x}".format(i, i))
