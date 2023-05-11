11a7ff2ac8746ed7a9ecdb01dcc8d9c9a39e0b04
mosesgitonga <infosec947@gmail.com>
2023-05-11 03:12:21 -0700

    from failurre

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
 		print("{} arguments:".format(arg))
 
 	for index, arg in enumerate(sys.argv[1:], 1):
-		print("{:d}: {:d}".format(index, arg))
+		print("{:d}: {}".format(index, arg))
