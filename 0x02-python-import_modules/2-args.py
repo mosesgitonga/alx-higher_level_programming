d76e5c564dd031f45b73688ee48a4d39a6025943
mosesgitonga <infosec947@gmail.com>
2023-05-11 03:23:40 -0700

    :d trial

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ import sys
 if __name__ == "__main__":
     arg = len(sys.argv) - 1
     if arg < 1:
-        print("{} argument.".format(arg))
+        print("{:d} argument.".format(arg))
     else:
-        print("{} arguments:".format(arg))
+        print("{:d} arguments:".format(arg))
 
     for index, arg in enumerate(sys.argv[1:], 1):
         print("{:d}: {}".format(index, arg))
