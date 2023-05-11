56abbf8fbf063e183344506a5b671dafaabb639d
mosesgitonga <infosec947@gmail.com>
2023-05-11 03:16:52 -0700

    pycode styling fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+import sys
 
 if __name__ == "__main__":
-	import sys
-	arg = len(sys.argv) - 1
-	if arg < 1:
-		print("{} argument.".format(arg))
-	else:
-		print("{} arguments:".format(arg))
+    arg = len(sys.argv) - 1
+    if arg < 1:
+        print("{} argument.".format(arg))
+    else:
+        print("{} arguments:".format(arg))
 
-	for index, arg in enumerate(sys.argv[1:], 1):
-		print("{:d}: {}".format(index, arg))
+    for index, arg in enumerate(sys.argv[1:], 1):
+        print("{:d}: {}".format(index, arg))
