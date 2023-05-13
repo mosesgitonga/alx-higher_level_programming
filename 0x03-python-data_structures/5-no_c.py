202521507354f5885d89f263327d60539bdbdb7e
mosesgitonga <infosec947@gmail.com>
2023-05-13 02:48:07 -0700

    chsanfe

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
     new_str = ""
     for char in my_string:
-        if char != 'c' and  char != 'C':
+        if char != 'c' and char != 'C':
             new_str = new_str + char
 
     return new_str
