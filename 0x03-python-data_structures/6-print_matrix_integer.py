5f6f982142560e19e5e9502faf1473c453647eb3
mosesgitonga <infosec947@gmail.com>
2023-05-13 23:13:16 -0700

    fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for raw in matrix:
         for index, value in enumerate(raw):
             if index != 0:
-                print("{}".format(" "), end =  " ")
-            print("{}".format(value), end = " ")
-        print()
+                print(" ", end = "")
+            print("{}".format(value), end = "")
+        print("")
