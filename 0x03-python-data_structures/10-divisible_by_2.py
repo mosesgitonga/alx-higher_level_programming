32a03dd48f6825c5eda9dbfe75f4a063f2d3a564
mosesgitonga <infosec947@gmail.com>
2023-05-15 01:57:03 -0700

    dividibility

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    newlist = []
+    for num in my_list:
+        if num % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+    return newlist
