9d387dd597b3986c2afae7f479fbe570c90bad1d
mosesgitonga <infosec947@gmail.com>
2023-05-14 21:09:56 -0700

    still some pycode errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for raw in matrix:
-        for index, value in enumerate(raw):
-            if index != 0:
-                print(end = " ")
-            print("{}".format(value), end = "")
+        for index in range(len(raw)):
+            if index != len(raw) - 1:
+                print("{} ".format(raw[index]),end = "")
+            else:
+                print("{}".format(raw[index]),end = "")
         print()
