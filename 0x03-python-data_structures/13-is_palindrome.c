35c524a4be0adbf07197541b6e1595c1cb08bd37
mosesgitonga <infosec947@gmail.com>
2023-05-16 05:56:10 -0700

    linked list if it is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,70 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
-int num_of_nodes(listint_t *head)
-{
-//this is to count the numbers
-	listint_t *curr;
-	if (head == NULL)
-	{
-		return 0;
-	}
-
-	int count = 0;
-	curr = head;
-	while (curr != NULL)
-	{
-		count++;
-		curr = curr->next;
-	}
-	return count;
-
-}
 int is_palindrome(listint_t **head)
 {
-	listint_t *curr;
-//traversing the list to the middle
-	int list_middle;
-	curr = *head;
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
 
-	list_middle = num_of_nodes(*head) / 2;
-	int dict1[100], i = 0;//dict stores the values of the travesed nodes
-	while (i < list_middle)
-	{
-		dict1[i] = curr->n;
-		curr = curr->next;
-		i++;
-	}
-//reversing the linked list to the middle from last
-	int dict2[100];//dict2 is for storing the the data of the reversed node up to the middle.
-	listint_t *prev = NULL, *next;
-	
-	int j = list_middle - 1;
-	
-	while (j > list_middle)//loop from last to middle of list
-	{
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-		dict2[j] = prev->n;
-		j--;
-	}
-	//check if list is palindrome or not
-	for (i = 0; i != (list_middle * 2); i++)
-	{
-		if (dict1[i] != dict2[i])
-		{
-			printf("is  palindrome");
-		}
-		else
-		{
-			printf("is not palindrome");
-		}
-	}
-	return 0;
-	
+    listint_t *slow = *head;
+    listint_t *fast = *head;
 
+    // Move fast pointer by two nodes and slow pointer by one node
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
 
+    // Reverse the second half of the linked list
+    listint_t *prev = NULL;
+    listint_t *next = NULL;
+    while (slow != NULL)
+    {
+        next = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = next;
+    }
+
+    // Compare the first half and reversed second half
+    listint_t *first_half = *head;
+    listint_t *second_half = prev;
+    while (second_half != NULL)
+    {
+        if (first_half->n != second_half->n)
+            return 0;
+        first_half = first_half->next;
+        second_half = second_half->next;
+    }
+
+    return 1;
 }
 
