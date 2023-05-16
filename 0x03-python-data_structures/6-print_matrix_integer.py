b1ce0ce8b73439f2d152fda27f9857a3ed3f7cb3
mosesgitonga <infosec947@gmail.com>
2023-05-16 09:15:57 -0700

    d changes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for raw in matrix:
         for index in range(len(raw)):
             if index != len(raw) - 1:
-                print("{} ".format(raw[index]),end = "")
+                print("{:d} ".format(raw[index]),end = "")
             else:
-                print("{}".format(raw[index]),end = "")
+                print("{:d}".format(raw[index]),end = "")
         print()
