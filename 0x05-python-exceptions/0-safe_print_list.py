8c372e0905bbd8271b84596565aa1d6aae75eb98
mosesgitonga <infosec947@gmail.com>
2023-05-29 03:08:15 -0700

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for elem in my_list:
+        for elem in my_list[:x]:
             if isinstance(elem, int):
                 print("{:d}".format(elem), end = "")
+                
                 count += 1
 
                 if count == x:
