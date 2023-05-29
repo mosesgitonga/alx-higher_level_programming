0b452c23ff54fdd1b6c99d297259880c5417fda2
mosesgitonga <infosec947@gmail.com>
2023-05-29 03:24:26 -0700

    printng safe with pycodestyles

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for elem in my_list:
             if isinstance(elem, int):
-                print("{:d}".format(elem), end = "")
+                print("{:d}".format(elem), e="")
                 count += 1
                 if count == x:
                     break
