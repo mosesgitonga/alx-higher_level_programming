2cbdeaf6f32c4d5fc86b54859c977cbd3fe902e8
mosesgitonga <infosec947@gmail.com>
2023-05-30 03:05:05 -0700

    pycode

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""creating a class"""
+
 
 class Square:
     """Square class"""
     def __init__(self):
-       """Initialization method""" 
-       pass
+        """Initialization method"""
+        pass
