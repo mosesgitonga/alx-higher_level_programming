0aa6a7d244f737ca7a5a70c90aa708234775668d
mosesgitonga <infosec947@gmail.com>
2023-05-30 09:14:23 -0700

    pip8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """class """
 
+
 class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
@@ -8,10 +9,12 @@ class Square:
 
     @property
     def size(self):
+        """Getter for size attribute."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Setter for size attribute."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -20,18 +23,22 @@ class Square:
 
     @property
     def position(self):
+        """Getter for position attribute."""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """Setter for position attribute."""
         if len(value) != 2 or not all(isinstance(num, int) and num >= 0 for num in value):
             raise ValueError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
+        """Calculate the area of the square."""
         return self.__size * self.__size
 
     def my_print(self):
+        """Print the square."""
         if self.__size == 0:
             print()
         else:
