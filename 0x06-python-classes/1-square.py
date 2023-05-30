fa61bacb981987ef87e48cbc9f02a0d4f73d8e01
mosesgitonga <infosec947@gmail.com>
2023-05-30 03:41:00 -0700

    pycode

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Square class"""
+
 
 class Square:
-    __size = None
+    """Represent a square"""
+
     def __init__(self, size):
+        """Initialization method
+        Args:
+            size (int): size of a new square
+        """
         self.__size = size
