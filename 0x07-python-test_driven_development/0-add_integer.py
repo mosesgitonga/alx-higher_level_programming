89e3609f060f004b1d04abc7d63a7e8dbfeeb56a
mosesgitonga <infosec947@gmail.com>
2023-06-04 06:57:03 -0700

    zeroth tak

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""function to add integers"""
+
+   
+def add_integer(a, b=98):
+    """Add integer
+    Args:
+        a (int): first int
+        b (int, optional): second int   
+    Returns: 
+        int: sum of integers
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return a + b
+
+if __name__=='__main__':
+    import doctest
+    doctest.testfile('tests/0-add_integer.txt')
