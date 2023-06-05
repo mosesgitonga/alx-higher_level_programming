b7b879744d48a02f0fb006d93d684a0ae122303b
mosesgitonga <infosec947@gmail.com>
2023-06-05 01:40:35 -0700

    dividing matrix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """function to add integers"""
 
-   
+
 def add_integer(a, b=98):
     """Add integer
     Args:
         a (int): first int
-        b (int, optional): second int   
-    Returns: 
+        b (int, optional): second int
+    Raises:
+        TypeError: if a and be are not int or float
+
+    Returns:
         int: sum of integers
     """
 
@@ -15,9 +18,9 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
     return a + b
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import doctest
     doctest.testfile('tests/0-add_integer.txt')
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Function to divide matrix"""
+
+
+def matrix_divided(matrix, div):
+    """Divide matrix
+    Args:
+        matrix: List of lists
+        div (int): Number to divide matrix by
+    Raises:
+        TypeError: If matrix is not a matrix (list of lists) of integers/floats
+        TypeError: If rows in the matrix have different sizes
+        TypeError: If div is not an int
+        ZeroDivisionError: If div is zero
+    Returns:
+        new_matrix: Matrix after division
+    """
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)\
+                of integers/floats")
+
+    row_sizes = [len(row) for row in matrix]
+    if not all(size == row_sizes[0] for size in row_sizes):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    for row in matrix:
+        new_row = [round(column / div, 2) for column in row]
+        new_matrix.append(new_row)
+
+    return new_matrix
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
