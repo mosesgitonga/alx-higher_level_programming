2eef576570057e9228416a60124cbba9d7e30fc5
mosesgitonga <infosec947@gmail.com>
2023-06-06 06:20:02 -0700

    pycodestyle

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""Locked Class module"""
+
 
 class LockedClass:
+    """class with only one attribute"""
+
     __slots__ = ['first_name']
 
     def __setattr__(self, name, value):
+        """this is  setatrrirbute"""
+
         x = "'LockedClass' object has no attribute '{}'".format(name)
         if name != 'first_name':
             raise AttributeError(x)
