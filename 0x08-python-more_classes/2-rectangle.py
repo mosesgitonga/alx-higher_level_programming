fad36a0b5a5436004655a24519f484f7fa96aee9
mosesgitonga <infosec947@gmail.com>
2023-06-06 22:41:48 -0700

    height changed

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -52,9 +52,9 @@ class Rectangle:
         """
 
         if not isinstance(value, int):
-            TypeError("width must be an integer")
+            TypeError("height must be an integer")
         if value < 0:
-            ValueError("width must be >= 0")
+            ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
