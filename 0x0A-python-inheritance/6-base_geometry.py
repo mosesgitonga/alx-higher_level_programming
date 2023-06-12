04959081556b64e0e6e377a55d97d3027514074d
mosesgitonga <infosec947@gmail.com>
2023-06-12 06:09:45 -0700

    exception added

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry:
+    """intializing class"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
