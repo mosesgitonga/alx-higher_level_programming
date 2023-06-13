1ed750dd9564a539db4cd65f42f594c45019443c
mosesgitonga <infosec947@gmail.com>
2023-06-13 04:45:28 -0700

    fixing docstring

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,13 +3,25 @@ import json
 
 
 def to_json_string(my_obj):
-    """
-    Convert a Python object to a JSON-formatted string.
+    """Convert a Python object to a JSON-formatted string.
 
     Args:
         my_obj: The Python object to be converted.
 
     Returns:
+        A JSON-formatted string representation of the object.
+    """
+    """
+    This function converts a Python object to a JSON-formatted string.
+
+    Parameters
+    ----------
+    my_obj : object
+        The Python object to be converted.
+
+    Returns
+    -------
+    str
         A JSON-formatted string representation of the object.
     """
     return json.dumps(my_obj)
