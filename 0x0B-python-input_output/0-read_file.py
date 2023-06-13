800a18535ff5a35e4c73420a7e5b2f2616c036b6
mosesgitonga <infosec947@gmail.com>
2023-06-13 03:09:40 -0700

    added docstring

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 
+"""
+Reads a text file (UTF8) and returns the contents of the file as a string.
+
+Args:
+    filename: The path of the file to read.
+
+Returns:
+    The contents of the file as a string.
+"""
 
 def read_file(filename=""):
-    """function to read file
-        Args:
-            filename: the file to read
+    """
+    Reads a text file (UTF8) and returns the contents of the file as a string.
 
+    Args:
+        filename: The path of the file to read.
+
+    Returns:
+        The contents of the file as a string.
     """
 
-    with open(filename, mode="r", encoding="utf-8") as my_file:
-        print(my_file.read())
+    with open(filename, mode="r", encoding="utf-8") as myfile:
+        print(myfile.read())
