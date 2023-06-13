5b7f8ee807e5b5e0177dd8fb591ecd0cb6994aee
mosesgitonga <infosec947@gmail.com>
2023-06-13 10:03:46 -0700

    dictionary

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""importing json"""
+import json
+
+
+def class_to_json(obj):
+    """class to json
+    Args:
+        obj: object in class
+    Return: dictionary
+    """
+    return obj.__dict__
