3f3d2d82e6a088f9a9d4b8400abc0ecac3f75515
mosesgitonga <infosec947@gmail.com>
2023-06-13 08:43:43 -0700

    save to json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""adding items to a list"""
+from json import dump, load
+from os import path
+from typing import List
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+def add_item_to_list(items: List[str], filename: str):
+    """Add items to a list and save it to a JSON file.
+
+    Args:
+        items (List[str]): The items to add to the list.
+        filename (str): The name of the JSON file to save the list.
+
+    Returns:
+        None
+    """
+    if path.exists(filename):
+        existing_list = load_from_json_file(filename)
+    else:
+        existing_list = []
+
+    existing_list.extend(items)
+
+    save_to_json_file(existing_list, filename)
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    add_item_to_list(args, "add_item.json")
