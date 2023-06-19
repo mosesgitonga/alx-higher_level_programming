996ca6447411cdc8bbd9073268705f1c6de1f06d
mosesgitonga <infosec947@gmail.com>
2023-06-19 06:51:07 -0700

    all changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """class base """
+import json
 
 
 class Base:
@@ -31,9 +32,73 @@ class Base:
         Returns:
             None
         """
-        self.id = id
+
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        x = "[]"
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return x
+        else:
+            jsonDict = json.dumps(list_dictionaries)
+            return jsonDict
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        json_list = []
+        if list_objs is not None:
+            json_list = [obj.to_dictionary() for obj in list_objs]
+
+        with open(filename, mode="w") as myfile:
+            myfile.write(cls.to_json_string(json_list))
+
+    def from_json_string(json_string):
+        x = "[]"
+        if json_string is None:
+            return x
+        else:
+            real_data = json.loads(json_string)
+            return real_data
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1, 1)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    def update(self, *args, **kwargs):
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        attrs = ["id", "width", "height", "x", "y"]
+        return {attr: getattr(self, attr) for attr in attrs}
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, mode="r") as myfile:
+                json_str = myfile.read()
+                if json_str:
+                    dictionaries = cls.from_json_string(json_str)
+                    instances = [cls.create(**dictionary) for dictionary in dictionaries]
+                    return instances
+                else:
+                    return []
+        except FileNotFoundError:
+            return []
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(Base):
     Represents a rectangle with width, height, position (x, y), and an ID.
     """
 
-    __nb_objects = 0
+    __nb_objects = 1
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
@@ -25,15 +25,12 @@ class Rectangle(Base):
         Returns:
             None
         """
-        if id is not None:
-            self.id = id
-        else:
-            self.id = self.__class__.__nb_objects + 1
-            self.__class__.__nb_objects += 1
+
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        super().__init__(id)
 
     @property
     def width(self):
@@ -56,6 +53,10 @@ class Rectangle(Base):
         Returns:
             None
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -79,6 +80,10 @@ class Rectangle(Base):
         Returns:
             None
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -102,6 +107,10 @@ class Rectangle(Base):
         Returns:
             None
         """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -125,4 +134,48 @@ class Rectangle(Base):
         Returns:
             None
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        func to find area of a rectanglke
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        symbol = "#"
+        res = ''
+        for _ in range(self.__y):
+            print()
+        for i in range(self.__height):
+            print(" " * self.x + "#" * self.width)
+
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+
+            for i, arg in enumerate(args):
+                if i < len(args):
+                    setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {"id" : self.id,
+                "width" : self.width,
+                "height" : self.height,
+                "x" : self.x,
+                "y" : self.y
+                }
+
+
+
