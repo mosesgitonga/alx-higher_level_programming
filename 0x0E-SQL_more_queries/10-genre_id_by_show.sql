f0d7e2070322076beea0a00bb87c02c7eea44826
mosesgitonga <infosec947@gmail.com>
2023-07-19 10:50:14 -0700

    bug fixed

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 -- script
-select tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.genre_id IS NOT NULL ORDER BY tv_shows.title, tv_show_genres.genre_id
