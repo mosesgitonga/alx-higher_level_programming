ca33c8f13475a473ae50a34a01c648112d7b12b2
mosesgitonga <infosec947@gmail.com>
2023-07-19 10:10:07 -0700

    ks

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- list all shows
+
+select tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
