37ee1560e74f83b4153c8dc6c0035188ec8ae5c8
mosesgitonga <infosec947@gmail.com>
2023-08-19 05:52:07 -0700

    added docstring

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+"""
+prints the first state in a table
+"""
+
 import sys
 from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True))
+    """
+    printng state
+    """
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True
+                )
+            )
 
     Session = sessionmaker(bind=engine)
     session = Session()
