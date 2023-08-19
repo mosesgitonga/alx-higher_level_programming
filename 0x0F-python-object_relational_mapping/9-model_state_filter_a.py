f8864c61198b0287105de0e21a3c42fe03d72567
mosesgitonga <infosec947@gmail.com>
2023-08-19 06:37:59 -0700

    fixed docstring

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,17 +7,22 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-
-engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True
+if __name__ == "__main__":
+    """
+    create engine
+    """
+    engine = create_engine(
+           "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True
+                )
             )
-        )
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    states_with_a = session.query(State).filter(
+            State.name.like('%a%')
+            ).order_by(State.id)
 
-for state in states_with_a:
-    print("{}: {}".format(state.id, state.name))
+    for state in states_with_a:
+        print("{}: {}".format(state.id, state.name))
