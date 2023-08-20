8ca84fe6c9e34bf4579be3d948d601969c6a1663
mosesgitonga <infosec947@gmail.com>
2023-08-20 01:32:10 -0700

    relationships

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+Base = declarative_base()
+
+
+class City(Base):
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True
+                )
+            )
+    Base.metadata.create_all(engine)
