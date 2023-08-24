d7e20664200a33c5923ee9ea7dd72d5aa689bdd5
mosesgitonga <infosec947@gmail.com>
2023-08-24 13:00:25 +0300

    deleted files

--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-function add(a, b) {
-  let res = a + b;
-  return res;
-}
-
-module.exports.add = add;
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-function add (a, b) {
-  const res = a + b;
-  return res;
-}
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-console.log(add(a, b));
