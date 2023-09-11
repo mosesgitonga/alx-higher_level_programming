ebded319b76c94da8b7121f88d1ddfeed9acdfc8
mosesgitonga <infosec947@gmail.com>
2023-09-11 07:30:23 -0700

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (target) {
+  if (target === 0 || target === 1) {
+    return 1;
+  } else {
+    return target * factorial(target - 1);
+  }
+}
+const target = parseInt(process.argv[2]);
+if (!isNaN(target)) {
+  const res = factorial(target);
+  console.log(res);
+}
