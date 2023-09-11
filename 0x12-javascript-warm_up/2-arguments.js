220cf50f5c447ae260e19dbca339f50f2e84fb01
mosesgitonga <infosec947@gmail.com>
2023-09-10 23:45:17 -0700

    arg count

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length === 3) {
+  console.log('Argument found');
+} else if (args.length >= 4) {
+  console.log('Arguments found');
+} else if (args.length <= 2) {
+  console.log('No argument found');
+}
