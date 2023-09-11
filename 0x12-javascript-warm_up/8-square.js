831c338c42e531bc789ebc0b5498af5746b2af94
mosesgitonga <infosec947@gmail.com>
2023-09-11 06:11:22 -0700

    more loops

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+const num = parseInt(arg[2]);
+
+if (!isNaN(num) && num >= 0) {
+  for (let i = 1; i <= num; i++) {
+    console.log('X'.repeat(num));
+  }
+}
