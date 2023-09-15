30696b1df60642f9fd2bd922ed40edc85d78e612
mosesgitonga <infosec947@gmail.com>
2023-09-15 03:50:18 -0700

    added a docstring

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""
+a script that lists all cities from the database hbtn_0e_4_usa
+"""
 
 import MySQLdb
 from sys import argv
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], host='localhost', port=3306, passwd=argv[2], db=argv[3])
 
-db = MySQLdb.connect(user=argv[1], host='localhost', port=3306, passwd=argv[2], db=argv[3])
+    cur = db.cursor()
 
-cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN\
+ states WHERE cities.state_id = states.id ORDER BY cities.id")
 
-cur.execute("SELECT * FROM cities ORDER BY cities.id")
-
-rows = cur.fetchall()
-for index, row in enumerate(rows):
-   print(index, row)
+    rows = cur.fetchall()
+    for row in rows:
+        print( row)
