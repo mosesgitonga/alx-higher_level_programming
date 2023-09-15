9cf1539c92c2f5a33bc72ebdeedee9cf634fb1d3
mosesgitonga <infosec947@gmail.com>
2023-09-14 22:46:09 -0700

    tabs identation fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,12 @@ lists all states in the db
 import MySQLdb
 from sys import argv
 if __name__ == '__main__':
-	db = MySQLdb.connect(
-	    host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-	)
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states ORDER BY states.id")
-	rows = cur.fetchall()
+    db = MySQLdb.connect(
+       host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+       )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
 
-	for row in rows:
-	    print(f"{row}")
+    for row in rows:
+        print(f"{row}")
