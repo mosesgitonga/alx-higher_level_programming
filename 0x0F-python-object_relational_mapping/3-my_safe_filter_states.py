ffe1a81a6243fac73c7e592c03a1fcda8cd93a43
mosesgitonga <infosec947@gmail.com>
2023-09-15 02:09:13 -0700

    preventing sql injectionby using %s

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,15 +3,16 @@
 from sys import argv
 import MySQLdb
 
-db = MySQLdb.connect(
-    host='localhost', user=argv[1], passwd=argv[2], db=argv[3]
-    )
-cur = db.cursor()
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost', user=argv[1], passwd=argv[2], db=argv[3]
+        )
+    cur = db.cursor()
 
-cur.execute(
-    "SELECT * FROM states WHERE name={} ORDER BY states.id".format(argv[4])
-    )
+    cur.execute(
+        "SELECT * FROM states WHERE \
+states.name = %s ORDER BY states.id", (argv[4],))
 
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
