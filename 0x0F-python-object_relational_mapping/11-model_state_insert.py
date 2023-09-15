c2a4dfc53e9b159bb95213c7f13cff5ebe5de793
mosesgitonga <infosec947@gmail.com>
2023-09-15 10:40:30 -0700

    added port number

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == '__main__':
     engine = create_engine(
-            "mysql+mysqldb: //{}: {}@localhost/{}".format(
+            "mysql+mysqldb: //{}: {}@localhost:3306/{}".format(
                 argv[1], argv[2], argv[3])
             )
     Session = sessionmaker(bind=engine)
