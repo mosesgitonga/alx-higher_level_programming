5182a947b3d363b549b068c4f16d25396419a76d
mosesgitonga <infosec947@gmail.com>
2023-09-15 01:30:54 -0700

    a script that lists all states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
         )
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
-       print(f"{row}")
+        print(f"{row}")
