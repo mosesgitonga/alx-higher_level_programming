53fb536d6a75725c3443deb741c22031b22e0a6b
mosesgitonga <infosec947@gmail.com>
2023-09-15 06:58:10 -0700

    import b4 calling correction

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,10 +6,6 @@ from sqlalchemy import create_engine, Integer, String, Column
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sys import argv
 
-engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
-        )
-
 Base = declarative_base()
 
 
