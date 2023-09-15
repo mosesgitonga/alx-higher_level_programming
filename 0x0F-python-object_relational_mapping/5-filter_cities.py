1b70b2a66f8e4758da4c075c6f07d74e8122d2f6
mosesgitonga <infosec947@gmail.com>
2023-09-15 05:30:34 -0700

    fixed missing docstring

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,17 +4,17 @@ all cities by state
 """
 import MySQLdb
 from sys import argv
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=argv[1], passwd=argv[2], host='localhost', port=3306, db=argv[3]
+        )
 
-db = MySQLdb.connect(
-    user=argv[1], passwd=argv[2], host='localhost', port=3306, db=argv[3]
-    )
+    cur = db.cursor()
 
-cur = db.cursor()
-
-cur.execute("SELECT cities.name FROM \
-    cities JOIN states ON cities.state_id = states.id\
-    WHERE states.name = %s ORDER BY cities.id ASC", (argv[4], ))
-cities = []
-for city in cur.fetchall():
-    cities.append(city[0])
-print(', '.join(cities))
+    cur.execute("SELECT cities.name FROM \
+        cities JOIN states ON cities.state_id = states.id\
+        WHERE states.name = %s ORDER BY cities.id ASC", (argv[4], ))
+    cities = []
+    for city in cur.fetchall():
+        cities.append(city[0])
+    print(', '.join(cities))
