f656f390f7cc29877bdb482aca4def634e3cae80
mosesgitonga <infosec947@gmail.com>
2023-09-15 01:56:54 -0700

    fixed pycodetyle issue of line length

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+        "SELECT * FROM states WHERE name\
+ LIKE BINARY 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
