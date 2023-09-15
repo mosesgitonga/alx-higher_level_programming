b5ac70d8852b2dc7d4bb4e41f23867d2d1d3f8b6
mosesgitonga <infosec947@gmail.com>
2023-09-15 08:35:22 -0700

    if state is not emtpy test case

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -22,3 +22,5 @@ if __name__ == '__main__':
         first_state = session.query(State).first()
 
         print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
