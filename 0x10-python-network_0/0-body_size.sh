d7985301da4fab4c18489e7ee300b5157188635e
mosesgitonga <infosec947@gmail.com>
2023-09-28 12:13:33 +0300

    lines must be 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 #displaying the size of body response
-response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-echo "$response"
+response=$(curl -s -o /dev/null -w "%{size_download}" "$1"); echo "$response"
