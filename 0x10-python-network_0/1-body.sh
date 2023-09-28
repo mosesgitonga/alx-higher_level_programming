80bb0e13b460431bb94ae22ad25700611e0c88e3
mosesgitonga <infosec947@gmail.com>
2023-09-28 14:24:13 +0300

    newline

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #script that returns the body of a response
-response=$(curl -s -w "%{http_code}" "$1"); if [ "${response: -3}" -eq 200 ]; then echo "$response" 
-fi
+response=$(curl -s -w "%{http_code}" "$1"); 
+[ "${response: -3}" -eq 200 ] && echo "$response" 
