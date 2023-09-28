c02b8cf5b029bb517df84cb74cee2f2d014dc7b4
mosesgitonga <infosec947@gmail.com>
2023-09-28 18:08:12 +0300

    more documetation added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """
-finding the peak of a list
-    -> it must return a single number
+find peak
 """
+
+
 def find_peak(list_of_integers):
+    """
+    finding the peak of a list
+        -> it must return a single number
+    """
     list = list_of_integers
     for i in range(len(list)):
         if list[i] + 1 > list[i] and list[i] > list[i] - 1:
             return list[i]
         else:
             pass
-            
