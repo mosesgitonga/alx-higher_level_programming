93f885da9b52375b21987c0151b538bd155c4b26
mosesgitonga <infosec947@gmail.com>
2023-09-29 14:37:40 +0300

    changes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,11 +9,11 @@ from sys import argv
 if __name__ == "__main__":
     with urllib.request.urlopen(f"{argv[1]}") as response:
         email = {
-            "Your email is": f"{argv[2]}"
+            "Your email is": argv[2]
         }
         encoded_email = urllib.parse.urlencode(email)
-        encoded_email = encoded_email.encode('utf-8')
-        req = urllib.request.Request(f"{argv[1]}", encoded_email)
+        encoded_email = encoded_email.encode('ascii')
+        req = urllib.request.Request(argv[1], encoded_email)
         with urllib.request.urlopen(req) as response:
             res_content = response.read()
-            print(res_content)
+            print(res_content.decode('utf-8'), "replace")
