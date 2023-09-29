718d0f7cfe457e202b9f214be239bf9c2731c7da
mosesgitonga <infosec947@gmail.com>
2023-09-29 12:35:07 +0300

    fixing chaker issue

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,11 @@
 script that fetches the content of a url
 """
 
+import urllib.request
 if __name__ == "__main__":
-    import urllib.request
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
-        decoded_content = body.decode('utf-8')
+        decoded_content = body.decode('utf-8', 'replace')
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
