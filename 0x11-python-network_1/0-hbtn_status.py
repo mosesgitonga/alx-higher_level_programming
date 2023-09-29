b16391486bf7484bfab025f60519d4d88f2ea063
mosesgitonga <infosec947@gmail.com>
2023-09-29 14:05:50 +0300

    python script for a post request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,14 @@ script that fetches the content of a url
 
 import urllib.request
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        body = response.read()
-        decoded_content = body.decode('utf-8', 'replace')
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf-8 content: {}".format(decoded_content))
+    try:
+        with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+            body = response.read()
+            decoded_content = body.decode('utf-8', 'replace')
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf-8 content: {}".format(decoded_content))
+
+    except:
+        pass
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+python script that takes in a url
+"""
+
 import urllib.request
+import urllib.parse
 from sys import argv
-with urllib.request.urlopen
+if __name__ == "__main__":
+    with urllib.request.urlopen(f"{argv[1]}") as response:
+        email = {
+            "Your email is": f"{argv[2]}"
+        }
+        encoded_email = urllib.parse.urlencode(email)
+        encoded_email = encoded_email.encode('utf-8')
+        req = urllib.request.Request(f"{argv[1]}", encoded_email)
+        with urllib.request.urlopen(req) as response:
+            res_content = response.read()
+            print(res_content)
