6fed8afafa2bb417c9cf4e304dca32d46f5f2757
mosesgitonga <infosec947@gmail.com>
2023-09-29 15:50:33 +0300

    http error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,11 +5,12 @@ the content in the response"""
 
 if __name__ == "__main__":
     import urllib.request
+    from urllib.error import HTTPError
     from sys import argv
     try:
         url = argv[1]
         with urllib.request.urlopen(url) as response:
             body = response.read().decode('utf-8')
             print(body)
-    except urllib.error.HTTPError as e:
+    except HTTPError as e:
         raise Exception(f"Error code: {e.status}")
