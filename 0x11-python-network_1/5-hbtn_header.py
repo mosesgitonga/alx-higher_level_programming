6d1e6bcd2089065b59a93c30f40b4421679f0f96
mosesgitonga <infosec947@gmail.com>
2023-09-29 16:53:19 +0300

    id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""script to retutn id of a url"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    response = requests.get(argv[1])
+    print(response.headers.get('Request-Id'))
