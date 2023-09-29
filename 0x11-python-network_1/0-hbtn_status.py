2778d75491a8ea50159d0ecccb53b0240463fc71
mosesgitonga <infosec947@gmail.com>
2023-09-29 14:57:57 +0300

    task 0 change

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,13 +5,11 @@ script that fetches the content of a url
 
 import urllib.request
 if __name__ == "__main__":
-    try:
-        with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-            body = response.read()
-            decoded_content = body.decode('utf-8', 'replace')
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf-8 content: {}".format(decoded_content))
-    except:
-        pass
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        decoded_content = body.decode('utf-8', 'replace')
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf-8 content: {}".format(decoded_content))
