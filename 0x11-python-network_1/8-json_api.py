aff7d9eed02fb594b4a346de9b13cc9ca0acc30d
mosesgitonga <infosec947@gmail.com>
2023-09-29 19:51:55 +0300

    search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" a Python script that takes in a letter and sends a POST request
+ to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+    if len(argv) <= 1:
+        a = ""
+    else:
+        a = argv[1]
+    payload = {'q': a}
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data=payload)
+    try:
+        res = response.json()
+        if res:
+            print(f"<{res.headers.get('id')}]> <{response.headers.get('name')}>")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
