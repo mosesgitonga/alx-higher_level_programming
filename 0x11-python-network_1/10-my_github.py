44552bb625e6adcadf45d2ad6b4335e76863274a
mosesgitonga <infosec947@gmail.com>
2023-09-29 20:55:50 +0300

    getting my github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+taking github credentials to display my id
+using github API
+"""
+
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    url = "https://api.github.com/users/mosesgitonga"
+    response = requests.get(url, auth=auth)
+    print(response.json().get('id'))
