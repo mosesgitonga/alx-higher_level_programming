88a7a031f729ef1bc0a4ffb525e3bfda625d4555
mosesgitonga <infosec947@gmail.com>
2023-09-29 16:56:54 +0300

    finding requsts id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     import requests
     from sys import argv
     response = requests.get(argv[1])
-    print(response.headers.get('Request-Id'))
+    print(response.headers.get('X-Request-Id'))
