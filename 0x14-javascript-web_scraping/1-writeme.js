cd0432c7abd053495cc0a4b57d7e89e3cd00d05a
mosesgitonga <infosec947@gmail.com>
2023-10-24 12:57:26 +0300

    fixing semistandard format

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filename = process.argv[2];
-const data_to_write = process.argv[3];
+const dataToWrite = process.argv[3];
 
-fs.writeFile(filename, data_to_write, 'utf-8', (err) => {
-    if (err) {
-        console.log(err);
-    }
-    return;
+fs.writeFile(filename, dataToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
 });
