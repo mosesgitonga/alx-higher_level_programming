2f533b49087b7820784908d2c0a3f2174eeac4df
mosesgitonga <infosec947@gmail.com>
2023-10-24 14:35:21 +0300

    fixed code format

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 const movieID = process.argv[2];
-const base_url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+const baseURL = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-request.get(base_url, (err, response, body) => {
-    if (err) {
-        console.log(err);
-    }
-    const data = JSON.parse(body);
-    console.log(data.title);
+request.get(baseURL, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
 });
