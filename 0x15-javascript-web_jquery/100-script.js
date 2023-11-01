8dc0f171ab4f04ccb6e16020a88dcae40d71c691
mosesgitonga <infosec947@gmail.com>
2023-11-01 18:21:56 +0300

    change coloe

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const targetElement = document.querySelector('header');
+    targetElement.style.color = '#FF0000';
+})
+const targetElement = document.querySelector('header');
+targetElement.style.color = '#FF0000';
